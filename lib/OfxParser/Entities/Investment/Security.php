440346894b784e6c4f2be040afaaaec83bc77466
Judd Mowbray <jmowbray@vtmgroup.com>
2023-05-02 09:06:54 -0400

    Add missing updates to Security entity

--- lib/OfxParser/Entities/Investment/Security.php
+++ lib/OfxParser/Entities/Investment/Security.php
@@ -2,11 +2,22 @@
 
 namespace OfxParser\Entities\Investment;
 
+use OfxParser\Entities\Investment;
+
 class Security extends Investment
 {
+    /**
+     * @var string
+     */
     public $uniqueId;
 
+    /**
+     * @var string
+     */
     public $name;
 
+    /**
+     * @var string
+     */
     public $ticker;
 }
